499996d60d22633107c43a7a38459954411198be
rtagliavia <rtagliavia.pp@gmail.com>
2020-11-15 13:58:27 -0400

    Updates

---
+++ precache-manifest.fe516d8d21bc94670fc344debe862bb1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6786cd9cf8801cd3d1a77b39af0bc355",
+    "url": "/react-rpg/index.html"
+  },
+  {
+    "revision": "3db7cc31a8cc1327e7e1",
+    "url": "/react-rpg/static/css/main.f02c015d.chunk.css"
+  },
+  {
+    "revision": "1b165eca3488eca1016f",
+    "url": "/react-rpg/static/js/2.cd4c0227.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-rpg/static/js/2.cd4c0227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3db7cc31a8cc1327e7e1",
+    "url": "/react-rpg/static/js/main.3f288ab7.chunk.js"
+  },
+  {
+    "revision": "798f62281de374e458a9",
+    "url": "/react-rpg/static/js/runtime-main.89bad45e.js"
+  },
+  {
+    "revision": "32e786286bcaa6ee44e841c7c194dbae",
+    "url": "/react-rpg/static/media/bat.32e78628.png"
+  },
+  {
+    "revision": "1e963f254321a100f0ae3eecb6a417fb",
+    "url": "/react-rpg/static/media/healingPotion.1e963f25.png"
+  },
+  {
+    "revision": "f194e7e20668eddc3ebd22fdb3b31ffa",
+    "url": "/react-rpg/static/media/manaPotion.f194e7e2.png"
+  },
+  {
+    "revision": "424b02a95f8a9cbfeaf00e9707630bf4",
+    "url": "/react-rpg/static/media/thief.424b02a9.jpg"
+  },
+  {
+    "revision": "6d2802ce8ffa04bbd98210be847a7c10",
+    "url": "/react-rpg/static/media/undead.6d2802ce.png"
+  }
+]);
